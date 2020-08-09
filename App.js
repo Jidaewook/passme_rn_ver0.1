1d6fa0845dec09692bc9d55b962191ff9f1692f2
Jidaewook <psatdoctor@naver.com>
2020-08-09 15:32:10 +0900

    #SigninScreen

--- App.js
+++ App.js
@@ -3,11 +3,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import SplashScreen from './screens/Splashscreen';
 import SignupScreen from './screens/Signupscreen';
+import SigninScreen from './screens/Signuinscreen';
 
 export default function App() {
   return (
     <>
-    <SignupScreen />
+    <SigninScreen />
     </>
   );
 }
